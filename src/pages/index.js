754da7b409b27c2ef2cb94013953fbb9d5afe511
hannarosenfeld <hannazrosenfeld@gmail.com>
2022-01-21 14:58:49 -0500

    Add toggle.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,117 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { BiChevronDown } from "react-icons/bi";
 
 import Navbar from "../components/NavBar.js"
+
 import musikschule_gebaeude from "../images/Foto_01.jpg"
 
 
 const rows = [
     {
-	text: "Verantwortliche",
+	id: 1,
+	title: "Verantwortliche",
+
     },
     {
-	text: "Lehrende der Zupfinstrumente",
+	id: 2,	
+	title: "Lehrende der Zupfinstrumente",
     },
     {
-	text: "Lehrende der Blechblasinstrumente",
+	id: 3,	
+	title: "Lehrende der Blechblasinstrumente",
     },
     {
-	text: "Lehrende des Tasteninstruments",
+	id: 4,	
+	title: "Lehrende des Tasteninstruments",
     },
     {
-	text: "Lehrende des Gesangs",
+	id: 5,	
+	title: "Lehrende des Gesangs",
     },
     {
-	text: "Lehrende des Schlagzeugs",
+	id: 6,	
+	title: "Lehrende des Schlagzeugs",
     },
     {
-	text: "Lehrende des Akkordeons",
+	id: 7,	
+	title: "Lehrende des Akkordeons",
     },
     {
-	text: "Lehrende der Musiktheorie",
+	id: 8,	
+	title: "Lehrende der Musiktheorie",
     },
     {
-	text: "Lehrende der Früherziehung",
+	id: 9,	
+	title: "Lehrende der Früherziehung",
     }
 ]
 
-const IndexPage = () => {
-
-    return (
+const RowContent = () => {
+    return(
 	<div>
-	<Navbar />
-	<div className="willkommen" width="300">
-	</div>
-	<img src={musikschule_gebaeude} style={{width: "100%"}}/>
-	<section className="paedagogen-und-verwaltung mt-5">
-	<div className="container d-flex flex-column">
-	<h1>PÄDAGOGEN & VERWALTUNG</h1>
-	<div className="dropdown-rows">
-	{rows.map(row => (
-	    <div className="row">
-		<div className="col">{row.text}</div>
-		<div className="col"><BiChevronDown style={{float: "right"}}/></div>
+	    <div>
+		<img src={{}} alt={{}} />
 	    </div>
-        ))}
-	</div>
-	</div>
-	</section>
+	    <div>
+		<h3> </h3>
+	    </div>
 	</div>
     )
-	}
+}
+
+class DropDownRows extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {isToggleOn: true};
+	// This binding is necessary to make `this` work in the callback
+	this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+	this.setState(prevState => ({
+	    isToggleOn: !prevState.isToggleOn
+	}));
+    }
+    render() {
+	return (
+	    <div className="dropdown-rows">
+	    {rows.map(row => (
+		<div key={row.id}>
+		    <div className="row">
+			<div className="col">{row.title}</div>
+			<div className="col">
+			    <BiChevronDown
+			    onClick={this.handleClick}
+			    style={{float: "right"}}/>
+			</div>
+			<div>
+			</div>
+		    </div>
+		    {this.state.isToggleOn ? <RowContent /> : ''}
+		</div>
+	    ))}
+
+	</div>
+	)
+    }
+}
+
+
+const IndexPage = () => {
+    return (
+	<div>
+	    <Navbar />
+	    <div className="willkommen" width="300">
+	    </div>
+	    <img src={musikschule_gebaeude} style={{width: "100%"}}/>
+	    <section className="paedagogen-und-verwaltung mt-5">
+		<div className="container d-flex flex-column">
+		    <h1>PÄDAGOGEN & VERWALTUNG</h1>
+		    <DropDownRows />
+		</div>
+	    </section>
+	</div>
+    )
+}
 
 export default IndexPage
