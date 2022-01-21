09521e416fefcd2f04715a93bb2c7df958d454c3
hannarosenfeld <hannazrosenfeld@gmail.com>
2022-01-21 13:08:23 -0500

    Add PÄDAGOGEN & VERWALTUNG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,64 @@
 import * as React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { BiChevronDown } from "react-icons/bi";
 
 import Navbar from "../components/NavBar.js"
-
 import musikschule_gebaeude from "../images/Foto_01.jpg"
+
+
+const rows = [
+    {
+	text: "Verantwortliche",
+    },
+    {
+	text: "Lehrende der Zupfinstrumente",
+    },
+    {
+	text: "Lehrende der Blechblasinstrumente",
+    },
+    {
+	text: "Lehrende des Tasteninstruments",
+    },
+    {
+	text: "Lehrende des Gesangs",
+    },
+    {
+	text: "Lehrende des Schlagzeugs",
+    },
+    {
+	text: "Lehrende des Akkordeons",
+    },
+    {
+	text: "Lehrende der Musiktheorie",
+    },
+    {
+	text: "Lehrende der Früherziehung",
+    }
+]
+
 const IndexPage = () => {
+
     return (
 	<div>
-	    <Navbar />
-	    <div className="willkommen" width="300">
-
+	<Navbar />
+	<div className="willkommen" width="300">
 	</div>
-	    <img src={musikschule_gebaeude} style={{width: "100%"}}/>	    
+	<img src={musikschule_gebaeude} style={{width: "100%"}}/>
+	<section className="paedagogen-und-verwaltung mt-5">
+	<div className="container d-flex flex-column">
+	<h1>PÄDAGOGEN & VERWALTUNG</h1>
+	<div className="dropdown-rows">
+	{rows.map(row => (
+	    <div className="row">
+		<div className="col">{row.text}</div>
+		<div className="col"><BiChevronDown style={{float: "right"}}/></div>
 	    </div>
+        ))}
+	</div>
+	</div>
+	</section>
+	</div>
     )
-}
+	}
 
 export default IndexPage
